c8ed46e22cdcbb71ab691861108b3fad8c62328d
Marcin Bereznicki <mbereznicki@student.agh.edu.pl>
2022-11-04 17:57:37 +0100

    Initial commit

---
+++ scripts/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from web_server import WebServer
+
+web_server = WebServer()
+web_server.foo()
---
+++ scripts/web_server.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class WebServer:
+    def __init__(self):
+        pass
+
+    def foo(self):
+        print("Hello world!")
