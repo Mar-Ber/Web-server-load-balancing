3a1516b380146c9d50d62591144b4f8522840ce6
baradejoo <baradziejkamil@gmail.com>
2022-11-11 16:06:33 +0100

    New file for math and refactor p.1

--- scripts/main.py
+++ scripts/main.py
@@ -4,15 +4,16 @@
 from web_server import WebServer
 
 buffer_size = 3         # param: K
-arrival_rate = 5      # param: lambda
+arrival_rate = 20      # param: lambda
 service_frequency = 30 # param: mi
-requests = 50      # param: N
-n = 3
+requests = 80   # param: N
+n = 3 # number of requests that the script calculates math info about queue
 
 try:
     web_server = WebServer(input_distribution_type="M", service_time_distribution_type="M", service_channels=1,
                            buffer_size=buffer_size, arrival_rate=arrival_rate, service_frequency=service_frequency,
-                           requests=requests, policy_type="shortest")
-    web_server.show_queueing_model(n)
+                           requests=requests, policy_type="random")
+    web_server.show_math_info(n, policy_type="random")
+    web_server.show_plots()
 except ValueError as ve:
     print("\n[ERROR] -> {0}".format(ve))
--- scripts/web_server.py
+++ scripts/web_server.py
@@ -5,6 +5,7 @@ import numpy as np
 from queue import Queue, QueueShortest
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from math_utils import *
 
 class WebServer:
     def __init__(self, input_distribution_type, service_time_distribution_type, service_channels, buffer_size,
@@ -21,95 +22,61 @@ class WebServer:
         self.service_time_distribution_type = service_time_distribution_type
         self.requests_arrival_times = self.calculate_requests_arrival_times()
         self.requests_service_times = self.calculate_requests_service_times()
-        self.requests_service_times[0] = [x+0.1 for x in self.requests_service_times[0]]
+        # self.requests_service_times[0] = [x+0.1 for x in self.requests_service_times[0]]
         self.queues = []
         self.choice = []
 
 
-        if self.policy_type == "random":
-            for i in range(self.queues_number):
-                self.queues.append(Queue(self.input_distribution_type, self.service_time_distribution_type,
-                                         self.requests_arrival_times[i], self.requests_service_times[i],
-                                         service_channels, buffer_size, arrival_rate/2,
-                                         service_frequency, int(requests/2)))
-        elif self.policy_type == "shortest":
-            self.shortest_queues = [QueueShortest(service_channels, int(requests/2)),
-                                    QueueShortest(service_channels, int(requests/2))]
+        # if self.policy_type == "random":
+        #     for i in range(self.queues_number):
+        #         self.queues.append(Queue(self.input_distribution_type, self.service_time_distribution_type,
+        #                                  self.requests_arrival_times[i], self.requests_service_times[i],
+        #                                  service_channels, buffer_size, arrival_rate/2,
+        #                                  service_frequency, int(requests/2)))
+        # elif self.policy_type == "shortest":
+        #     self.shortest_queues = [QueueShortest(service_channels, int(requests/2)),
+        #                             QueueShortest(service_channels, int(requests/2))]
 
-            ########
-            print(self.requests_arrival_times[0])
-            print(self.requests_service_times[0])
-            self.queue_size = [0]*int(requests/2)
-            self.queue_size2 = [0]*int(requests/2)
+        #     ########
+        #     print(self.requests_arrival_times[0])
+        #     print(self.requests_service_times[0])
+        #     self.queue_size = [0]*int(requests/2)
+        #     self.queue_size2 = [0]*int(requests/2)
 
-            for i in range(1, int(requests/2)):
-                if i == 1:
-                    print("Kolejka nr 1", i)
-                    self.shortest_queues[0].simulate_shortest_queue(self.requests_arrival_times[0][i], self.requests_service_times[0][i], i)
-                    self.queue_size[i]=self.shortest_queues[0].iterator
-                    self.queue_size2[i]=self.shortest_queues[1].iterator
-                    self.choice.append("1")
-                else:
+        #     for i in range(1, int(requests/2)):
+        #         if i == 1:
+        #             print("Kolejka nr 1", i)
+        #             self.shortest_queues[0].simulate_shortest_queue(self.requests_arrival_times[0][i], self.requests_service_times[0][i], i)
+        #             self.queue_size[i]=self.shortest_queues[0].iterator
+        #             self.queue_size2[i]=self.shortest_queues[1].iterator
+        #             self.choice.append("1")
+        #         else:
 
-                    if self.shortest_queues[0].iterator <= self.shortest_queues[1].iterator:
-                        print("Kolejka nr 1")
+        #             if self.shortest_queues[0].iterator <= self.shortest_queues[1].iterator:
+        #                 print("Kolejka nr 1")
 
-                        self.shortest_queues[0].simulate_shortest_queue(self.requests_arrival_times[0][i], self.requests_service_times[0][i], i)
-                        self.queue_size[i]=self.shortest_queues[0].iterator
-                        self.queue_size2[i]=self.shortest_queues[1].iterator
-                        self.choice.append("1")
-                    else:
-                        print("Kolejka nr 2")
-                        self.choice.append("2")
-                        self.shortest_queues[1].simulate_shortest_queue(self.requests_arrival_times[0][i], self.requests_service_times[0][i], i)
-                        self.queue_size2[i]=self.shortest_queues[1].iterator
-                        self.queue_size[i]=self.shortest_queues[0].iterator
+        #                 self.shortest_queues[0].simulate_shortest_queue(self.requests_arrival_times[0][i], self.requests_service_times[0][i], i)
+        #                 self.queue_size[i]=self.shortest_queues[0].iterator
+        #                 self.queue_size2[i]=self.shortest_queues[1].iterator
+        #                 self.choice.append("1")
+        #             else:
+        #                 print("Kolejka nr 2")
+        #                 self.choice.append("2")
+        #                 self.shortest_queues[1].simulate_shortest_queue(self.requests_arrival_times[0][i], self.requests_service_times[0][i], i)
+        #                 self.queue_size2[i]=self.shortest_queues[1].iterator
+        #                 self.queue_size[i]=self.shortest_queues[0].iterator
 
 
-            print(self.queue_size, self.queue_size2)
-            # QueueShortest(request_arrival_time, request_service_time,
-            #                              service_channels, request)]
-            ########
-        else:
-            print("fajne te kolejki")
+        #     print(self.queue_size, self.queue_size2)
+        #     # QueueShortest(request_arrival_time, request_service_time,
+        #     #                              service_channels, request)]
+        #     ########
+        # else:
+        #     print("fajne te kolejki")
 
-    def calculate_n_requests_probability(self, n):
-        if self.policy_type == "random":
-            if 0 <= n <= self.buffer_size and self.service_intensity != 1:
-                result = (1-self.service_intensity) * (self.service_intensity ** n) / \
-                         (1-self.service_intensity ** (self.buffer_size+1))
-                return round(result, 4)
-            else:
-                raise ValueError("Calculation not supported")
-        else:
-            raise ValueError("Policy type = '{0}' is not supported".format(self.policy_type))
 
-    def calculate_normalized_rejection_rate(self):
-        if self.policy_type == "random":
-            return self.calculate_n_requests_probability(n=self.buffer_size)
-        else:
-            raise ValueError("Policy type = '{0}' is not supported".format(self.policy_type))
 
-    def calculate_average_requests_number(self):
-        if self.policy_type == "random":
-            if int(self.service_intensity) == 1:
-                result = self.buffer_size / 2
-                return round(result, 4)
-            else:
-                result = self.service_intensity / (1-self.service_intensity) - \
-                         ((self.buffer_size+1) * (self.service_intensity ** (self.buffer_size+1)) /
-                          (1-(self.service_intensity ** (self.buffer_size+1))))
-                return round(result, 4)
-        else:
-            raise ValueError("Policy type = '{0}' is not supported".format(self.policy_type))
 
-    def calculate_average_response_time(self):
-        if self.policy_type == "random":
-            result = 2 * self.calculate_average_requests_number() * (1-self.service_intensity**(self.buffer_size+1)) / \
-                     (self.arrival_rate * (1 - self.service_intensity ** self.buffer_size))
-            return round(result, 4)
-        else:
-            raise ValueError("Policy type = '{0}' is not supported".format(self.policy_type))
 
     def calculate_requests_arrival_times_for_random_policy(self):
         if self.input_distribution_type == "M":
@@ -124,29 +91,18 @@ class WebServer:
       
     def calculate_requests_arrival_times(self):
         result = []
-        if self.policy_type == "shortest":
-            for _ in range(self.queues_number):
-                result.append(self.calculate_requests_arrival_times_for_random_policy())
-            return result
+        for _ in range(self.queues_number):
+            result.append(self.calculate_requests_arrival_times_for_random_policy())
+        return result
       
-
     def calculate_requests_service_times(self):
         result = []
-        if self.policy_type == "shortest":
-            for _ in range(self.queues_number):
-                result.append(self.calculate_requests_service_time_for_random_policy())
-            return result
+        for _ in range(self.queues_number):
+            result.append(self.calculate_requests_service_time_for_random_policy())
+        return result
       
 
-    def print_calculated_values(self, n):
-        if self.policy_type == "random":
-            print("\nProbability of {0} requests in the system = {1}".format(n,
-                                                                             self.calculate_n_requests_probability(n)))
-            print("Normalized rejection rate = {0}".format(self.calculate_normalized_rejection_rate()))
-            print("Average requests number in the system = {0}".format(self.calculate_average_requests_number()))
-            print("Average system response time = {0}\n".format(self.calculate_average_response_time()))
-        else:
-            raise ValueError("Policy type = '{0}' is not supported".format(self.policy_type))
+
 
     def plot_queues(self):
         if self.policy_type == "random":
@@ -183,9 +139,21 @@ class WebServer:
         plt.ylabel("value")
         plt.show()
 
-    
 
-    def show_queueing_model(self, n=5):
-        # self.print_calculated_values(n)
+    def show_plots(self):
+        pass
         # self.plot_queues()
-        self.plot_nasze()
+        # self.plot_nasze()
+    
+    def show_math_info(self, n, policy_type):
+        if policy_type == "random":
+            print("\nProbability of {0} requests in the system = {1}".
+                                        format(n, calculate_n_requests_probability(n, self.buffer_size, self.service_intensity)))
+            print("Normalized rejection rate = {0}".
+                                        format(calculate_normalized_rejection_rate(self.buffer_size, self.service_intensity)))
+            print("Average requests number in the system = {0}".
+                                        format(calculate_average_requests_number(self.buffer_size, self.service_intensity)))
+            print("Average system response time = {0}\n".
+                                        format(calculate_average_response_time(self.buffer_size, self.service_intensity, self.arrival_rate)))
+        else:
+            raise ValueError("Policy type = '{0}' is not supported.".format(policy_type))
